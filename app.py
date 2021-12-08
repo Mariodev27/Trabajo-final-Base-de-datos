29d95c69e6065e2f2ba9d43be1894cf8b2a113b3
MarioBTiger <85888524+MarioBTiger@users.noreply.github.com>
2021-12-08 00:07:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, render_template, redirect
-
+from pymysql import Connection
+from pymysql import cursors
+from pymysql.connections import Connection
+from pymysql.cursors import Cursor
 from controller import insert_employee, get_employees
+from db import get_connection_db
 #debo decirle a mi app que pueda cargar la carpeta
 app=Flask(__name__, template_folder="templates")
 
@@ -13,6 +17,28 @@ def home():
 @app.route('/crear')
 def create():
     return render_template('create.html')
+
+@app.route('/eliminar/<int:Id>')
+def eliminar(Id):
+    Connection=get_connection_db()
+    Cursor.execute("DELETE from empleados WHERE Id=%s",(Id))
+    Connection.commit()
+    return redirect('/')
+
+@app.route('/editar/<int:Id>')
+def editar(Id):
+    Connection=get_connection_db()
+    Cursor.execute(
+        "SELECT * FROM empleados WHERE Id=%s",(Id))
+    employees = Cursor.fetchall()
+    Connection.commit()
+    print(employees)
+    return render_template('empleados/editar.html',employees=employees)
+
+@app.route('/registrarse')
+def registrarse():
+    employees = get_employees()
+    return render_template('index2.html', employees=employees)
 
 @app.route('/store-employee', methods=['POST'])
 def store():
